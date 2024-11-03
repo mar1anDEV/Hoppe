6d6fdeead8fe6d33cc5811cb51cc96efdd3cd09e
Marian Halibei <137928134+mar1anDEV@users.noreply.github.com>
2024-11-04 00:20:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('closeMenu').addEventListener('click', function(){
         document.querySelector('.navbar-collapse-ui').style.display = 'none';
     })
+
+    let userInput = document.querySelector('.search-input');
+    userInput.addEventListener('input', function(){
+        const DieselLink = "https://hoppe.parts/sortiment/dieseltanks.html";
+        const validInputDiesel = ['dieseltank','DIESELTANK','Dieseltank'];
+        if(validInputDiesel.includes(userInput.value)){
+            window.location.href = DieselLink;
+        }
+        const HydraulikLink = "https://hoppe.parts/sortiment/hydraulik/hydrauliktanks.html";
+        const validInputHydraulik = ['hydrauliktank','hydraulik','Hydrauliktank','Hydrauliktanks','HYDRAULIKTANKS'];
+        if(validInputHydraulik.includes(userInput.value)){
+            window.location.href = HydraulikLink;
+        } 
+        const staukästenLink = "https://hoppe.parts/sortiment/staukasten.html"
+        const validStaukästenInput = ["Staukästen","STAUKÄSTEN","staukästen"];
+        if(validStaukästenInput.includes(userInput.value)){
+            window.location.href = staukästenLink;
+        } 
+    })
 });
+
